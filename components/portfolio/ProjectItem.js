6d8ef8f3c3c0973772f20ab7d5d0a7e93a6cb3e3
4kpros <prosper.abouar@gmail.com>
2022-05-19 01:37:57 +0100

    Images update

--- components/portfolio/ProjectItem.js
+++ components/portfolio/ProjectItem.js
@@ -1,6 +1,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
+import { LazyMotion, domAnimation, m } from 'framer-motion'
 
 const ProjectItem = (props) => {
 
@@ -12,7 +13,22 @@ const ProjectItem = (props) => {
                 {
                     project ?
                         <div className="w-full">
-                            <Image className="w-full h-72 object-cover" width={450} height={288} layout="responsive" placeholder="blur" blurDataURL={project.cover_art ? project.cover_art : "/images/backgrounds/default-background_small.webp"} src={project.cover_art ? project.cover_art : "/images/backgrounds/default-background_small.webp"} alt={project.name ? project.name : `default-project-name`}/>
+                            <LazyMotion features={domAnimation}>
+                                <m.div
+                                    className="border border-my-orage-color"
+                                    whileHover={{scale:1.025}}
+                                >
+                                    <Image className="w-full h-auto object-cover m-0 p-0 bg-my-footer-color" 
+                                        width={450} 
+                                        height={288} 
+                                        layout="responsive" 
+                                        placeholder="blur" 
+                                        blurDataURL={project.cover_art ? project.cover_art : "/images/backgrounds/default-background_small.webp"} 
+                                        src={project.cover_art ? project.cover_art : "/images/backgrounds/default-background_small.webp"} 
+                                        alt={project.name ? project.name : `default-project-name`}
+                                    />
+                                </m.div>
+                            </LazyMotion>
                         </div>
                     :
                         <div className="w-full">
