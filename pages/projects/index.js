e4fbc145b57ebb2c10f7d6fe6289bf3a94f09591
4kpros <prosper.abouar@gmail.com>
2022-10-21 22:13:35 +0100

    Add projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -15,29 +15,30 @@ export default function Projects(){
 
     return (
         <Layout title={title} description={subtitle}>
-            <div className="w-full text-white">
-                <section className="w-full pt-20 min-h-screen">
-                    <div className="w-full max-w-screen-xl mx-auto px-4">
-                        <div className="w-full flex flex-wrap md:flex-nowrap justify-start items-center mt-12">
-                            {
-                                projects && projects.length > 0 ?
-                                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 min-h-40">
-                                        {
-                                            projects.map((project, index) => {
-                                                return(
-                                                    <ProjectItem project={project} key={index}/>
-                                                )
-                                            })
-                                        }
-                                    </div>
-                                :
-                                    <p className="text-xl text-my-gray-color">
-                                        Aucun projet pour l&apos;instant !
-                                    </p>
-                            }
+            <div className="w-full max-w-screen-xl mx-auto px-8 py-8">
+                <div className="w-full px-4 md:px-8 pb-12 text-white text-xl ">
+                    <section className="w-full py-5">
+                        <div className='w-auto font-bold text-2xl mb-8'>
+                            Liste de quelques projets personnels et d'apprentissage
                         </div>
-                    </div>
-                </section>
+                        {
+                            projects && projects.length > 0 ?
+                                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 min-h-40 mt-4">
+                                    {
+                                        projects.map((project, index) => {
+                                            return(
+                                                <ProjectItem project={project} key={index}/>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            :
+                                <p className="text-xl text-my-gray-color mt-8">
+                                    Aucun projet pour l&apos;instant !
+                                </p>
+                        }
+                    </section>
+                </div>
             </div>
         </Layout>
     );
