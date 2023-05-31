96c236fb337c12ed9477652d8c4b2d02ff171e85
aprospere <aprospere@irex.aretex.ca>
2023-05-31 06:37:29 +0100

    Fix 404 error page

--- pages/404.js
+++ pages/404.js
@@ -17,39 +17,37 @@ export default function Custom404() {
     return (
         <Layout title={pageTile} description={pageDescription}>
             <div className="w-full text-my-text-color">
-                <section className="w-full">
-                    <div className="w-full max-w-screen-xl mx-auto px-4">
-                        <div className="w-full min-h-[70vh] flex items-center justify-center">
-                            <div className="w-auto flex flex-col items-center m-auto">
-                                <div className="w-128 h-128">
-                                    <Image 
-                                        width={128} 
-                                        height={128} 
-                                        placeholder="blur" 
-                                        blurDataURL="/images/svg/alerts/warn.webp" 
-                                        src="/images/svg/alerts/warn.webp" 
-                                        alt="404-img"
-                                        className='object-contain'
-                                        />
-                                </div>
-                                <h3 className="text-center font-semibold">
-                                    {error}
-                                </h3>
-                                <p className="text-center text-my-text-color opacity-50 mt-4">
-                                    {errorMessage}
-                                </p>
-                                <div className="mt-12">
-                                    <Link 
-                                        href={'/'}
-                                        className="text-my-text-button-color font-bold bg-my-main-color rounded-lg px-6 py-2 text-center focus:outline-none"
-                                        >
-                                        {homePage}
-                                    </Link>
-                                </div>
+                <div className="w-full max-w-screen-xl mx-auto px-4">
+                    <div className="w-full h-screen flex items-center justify-center">
+                        <div className="w-auto flex flex-col items-center m-auto">
+                            <div className="w-128 h-128">
+                                <Image 
+                                    width={128} 
+                                    height={128} 
+                                    placeholder="blur" 
+                                    blurDataURL="/images/svg/alerts/warn.webp" 
+                                    src="/images/svg/alerts/warn.webp" 
+                                    alt="404-img"
+                                    className='object-contain'
+                                    />
+                            </div>
+                            <h3 className="text-center font-semibold">
+                                {error}
+                            </h3>
+                            <p className="text-center text-my-text-color opacity-50 mt-2">
+                                {errorMessage}
+                            </p>
+                            <div className="mt-6">
+                                <Link 
+                                    href={'/'}
+                                    className="w-auto text-white text-center bg-black/50 px-6 py-2 hover:shadow-lg hover:bg-black/70 transition-all"
+                                    >
+                                    {homePage}
+                                </Link>
                             </div>
                         </div>
                     </div>
-                </section>
+                </div>
             </div>
         </Layout>
     )
