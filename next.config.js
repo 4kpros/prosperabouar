f88dd1b874f1b7a0b0d1c644d3da94750fd172b8
4kpros <prosper.abouar@gmail.com>
2025-09-11 14:03:25 +0100

    fix: issues

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     unoptimized: true,
   },
   i18n: {
     // providing the locales supported by your application
     locales: ["en", "fr"],
-   //  default locale used when the non-locale paths are visited
-   defaultLocale: "en",
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: "en",
   },
-}
+};
 
-const nextTranslate = require('next-translate')
+const nextTranslate = require("next-translate");
 
-module.exports = nextTranslate()
-
+module.exports = nextTranslate();
